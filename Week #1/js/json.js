e8c2e5b20a3d2e1cb1b599960233a96f3885617d
Tim Cook <redeemingdesign@gmail.com>
2012-08-28 12:11:39 -0500

    Start

---
+++ Week #1/js/json.js
@@ -0,0 +1,27 @@
+/*		JSON data
+
+*/
+
+
+var json = {
+	"car1": {
+		"make": 		["Make: ", "Porsche"],
+		"model": 		["Model: ", "911"],
+		"year": 		["Year: ", "2009"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Black"],
+		"display": 		["What makes it stand out? ", ["Sound", "Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "I like it much."]
+	},
+	"car2": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "I like it muchly."]
+	}
+}
