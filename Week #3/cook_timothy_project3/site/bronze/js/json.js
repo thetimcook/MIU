26926ce64088b7e1cff1a50f203ce53e49f93e20
Tim Cook <redeemingdesign@gmail.com>
2012-09-10 21:28:45 -0500

    Fixed links and added stock form to bronze page

---
+++ Week #3/cook_timothy_project3/site/bronze/js/json.js
@@ -0,0 +1,207 @@
+/*		JSON data
+
+*/
+
+
+var json = {
+	"car1": {
+		"make": 		["Make: ", "Porsche"],
+		"model": 		["Model: ", "911"],
+		"year": 		["Year: ", "2009"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Black"],
+		"display": 		["What makes it stand out? ", ["Sound", "Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque. Cras egestas lacinia erat, id scelerisque augue dictum vitae. Aliquam erat volutpat. Praesent mattis, lectus et venenatis bibendum, velit sem interdum elit, nec varius magna eros sed eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."]
+	},
+	"car2": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque."]
+	},
+	"car3": {
+		"make": 		["Make: ", "Audi"],
+		"model": 		["Model: ", "R8"],
+		"year": 		["Year: ", "2010"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Silver"],
+		"display": 		["What makes it stand out? ", ["Fast"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car4": {
+		"make": 		["Make: ", "Cadillac"],
+		"model": 		["Model: ", "Escalade"],
+		"year": 		["Year: ", "2008"],
+		"doors": 		["Number of doors: ", "4"],
+		"colors": 		["Color: ", "Black"],
+		"display": 		["What makes it stand out? ", ["Sound", "Rims"]],
+		"condition": 	["Condition: ", "Not"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car5": {
+		"make": 		["Make: ", "BMW"],
+		"model": 		["Model: ", "7 Series"],
+		"year": 		["Year: ", "2011"],
+		"doors": 		["Number of doors: ", "4"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car6": {
+		"make": 		["Make: ", "BMW"],
+		"model": 		["Model: ", "M5"],
+		"year": 		["Year: ", "2009"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Silver"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car7": {
+		"make": 		["Make: ", "Audi"],
+		"model": 		["Model: ", "S4"],
+		"year": 		["Year: ", "2008"],
+		"doors": 		["Number of doors: ", "4"],
+		"colors": 		["Color: ", "White"],
+		"display": 		["What makes it stand out? ", ["Fast"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car8": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car9": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car10": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car11": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car12": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car13": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car14": {
+		"make": 		["Make: ", "Audi"],
+		"model": 		["Model: ", "R8"],
+		"year": 		["Year: ", "2010"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Silver"],
+		"display": 		["What makes it stand out? ", ["Fast"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car15": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car16": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car17": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car18": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car19": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	},
+	"car20": {
+		"make": 		["Make: ", "Ferrari"],
+		"model": 		["Model: ", "458"],
+		"year": 		["Year: ", "2012"],
+		"doors": 		["Number of doors: ", "2"],
+		"colors": 		["Color: ", "Red"],
+		"display": 		["What makes it stand out? ", ["Fast", "Rims"]],
+		"condition": 	["Condition: ", "Amazing"],
+		"describe": 	["Describe the car in your own words. ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non blandit enim. Pellentesque eu nunc purus. Ut gravida bibendum magna ac pellentesque.."]
+	}
+}
---
+++ Week #3/cook_timothy_project3/site/gold/js/main.js
@@ -0,0 +1,365 @@
+/*	main.js
+	Timothy Cook
+	Project 2
+	MiU Full Sail
+*/
+
+
+//wait until the DOM is ready
+window.addEventListener("DOMContentLoaded", function(){
+	//getElementById Function
+	var selectColor = ["--Choose A Color--", "Black", "White", "Silver", "Red", "Blue", "Yellow", "Green"],
+		selectMake = ["Make", "Acura", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Cadillac", "Chrysler", "Dodge", "Ferrari", "FIAT", "Ford", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Lotus", "Maserati", "Maybach", "Mazda", "McLaren", "Mercedes-Benz", "MINI", "Mitsubishi", "Nissan", "Porsche", "Ram", "Rolls-Royce", "Saab", "Scion", "Smart", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagon", "Volvo"],
+		conditionValue,
+		holdValues
+	;
+	
+	function ge(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+	//Create select field element and populate with options.
+	function pickColor(selectColor){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = ge('select');
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "colors");
+		for (var i=0, j=selectColor.length; i<j; i++){
+			var makeOption = document.createElement('option');
+			var optText = selectColor[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
+	
+	// Find value of selected radio button.
+
+	function getSelectedRadio(){
+		var radios = document.forms[0].condition;
+		for (var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				var conditionValue = radios[i].value;
+			}
+		}
+		return conditionValue;
+	}
+	function getCheckboxValue(){
+		var checkboxes = document.forms[0].display;
+		var holdValues = [];
+		for (var i=0, j=checkboxes.length; i<j; i++){
+			if(checkboxes[i].checked){ 
+				var checkedValue = checkboxes[i].value;
+				holdValues.push(checkedValue);
+			}
+		}
+		return holdValues;
+	}
+	
+/*
+	function toggleControls(n) {
+		switch(n) {
+			case "on":
+				ge('headerBar').innerHTML = "Tagged Cars";
+				ge('carForm').style.display = "none";
+				ge('clear').style.display = "inline";
+				ge('displayLink').style.display = "none";
+				ge('addNew').style.display = "inline";
+				break;
+			case "off":
+				ge('carForm').style.display = "block";
+				ge('clear').style.display = "inline";
+				ge('displayLink').style.display = "inline";
+				ge('addNew').style.display = "none";
+				ge('cars').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+*/
+	
+	function storeData(key) {
+		if (!key) {
+			var id			= Math.floor(Math.random()*1000000);
+		} else {
+			id = key;
+		}
+		//Gather all form field values and store in an object.
+		//Object properties contain array with form label and input values.
+		var condition = getSelectedRadio();
+		var display = getCheckboxValue();
+		var car				= {};
+			car.make 		= ["Make: ", ge('make').value];
+			car.model		= ["Model: ", ge('model').value];
+			car.year		= ["Year: ", ge('year').value];
+			car.doors		= ["Number of doors: ", ge('doors').value];
+			car.colors 		= ["Color: ", ge('colors').value];
+			car.display		= ["What makes it stand out? ", display];
+			car.condition	= ["Condition: ", condition];
+			car.describe	= ["Describe the car in your own words. ", ge('describe').value];
+		// Save data to local storage: Use Strinify to convert our object to a sting.
+		localStorage.setItem(id, JSON.stringify(car));
+		alert("Car Tagged!");
+	}
+	
+	function getData(){
+/* 		toggleControls("on"); */
+		if(localStorage.length === 0) {
+			alert("There are no cars in your Garage, so I went ahead and added a couple!");
+			autoFill();
+		}
+		
+		//Write Data from local storage to the browser.
+		var makeDiv = ge('cars');
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		makeList.setAttribute("data-role", "listview");
+		makeList.setAttribute("data-filter", "true");
+		makeList.setAttribute("data-inset", "false");
+/* 		document.body.appendChild(makeDiv);
+		ge('cars').style.display = "block"; */
+		for (var i=0, len=localStorage.length; i<len; i++){
+			var linksLi = document.createElement('li');
+			var makeSubList = document.createElement('li');
+			makeList.appendChild(makeSubList);
+			makeSubList.setAttribute("data-role", "collapsible");
+/*
+			makeLi.style.padding = "0px 0px 12px 0px"
+			makeLi.style.margin = "0px 0px 8px 0px";
+			makeLi.style.borderBottom = "1px white solid"
+*/	
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//Convert the string from local storage value back to an object.
+			var obj = JSON.parse(value);
+			
+			getLogo(obj.make[1], makeSubList);
+			for (var n in obj) {
+				var makeSubLi = document.createElement('p');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
+			}
+			var breakTag = document.createElement('br');
+			linksLi.appendChild(breakTag);
+			
+			makeItemLinks(localStorage.key(i), linksLi); //Create edit and delete buttons
+			
+			var breakTag = document.createElement('br');
+			linksLi.appendChild(breakTag);
+		}
+/*
+		makeDiv.style.margin		= "0px 0px 8px 0px";
+		makeList.style.fontSize		= "10pt";
+		makeList.style.margin		= "8px 8px 0px 8px";
+		makeList.style.background	= "rgba(109, 174, 218, 1.0)";
+		makeList.style.padding		= "8px 8px 12px 8px";
+*/
+	}
+	//Get logo for car make.
+
+
+
+	function getLogo(logo, makeSubList) {
+		var imageLi = document.createElement('h3');
+		makeSubList.appendChild(imageLi);
+		var newImg = document.createElement('img');
+		var setSrc = newImg.setAttribute("src", "images/"+ logo +".jpg");
+		imageLi.appendChild(newImg);
+	}
+
+	
+	//Auto fill data
+
+	function autoFill() {
+		for (var n in json) {
+			var id = Math.floor(Math.random()*1000000);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}
+
+	
+	//Create the edit and delete links for each item
+	function makeItemLinks(key, linksLi) {
+		//add edit single item link
+		var editLink = document.createElement('a');
+		editLink.setAttribute("data-role", "button");
+		editLink.setAttribute("data-icon", "gear");
+		editLink.setAttribute("data-inline", "true");
+		editLink.setAttribute("data-theme", "b");
+		editLink.href = "additem.html#";
+		editLink.key = key;
+		var editText = "Edit Car";
+		editLink.addEventListener("click", editCar);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+/*
+		var breakTag = document.createElement('br');
+		linksLi.appendChild(breakTag);
+*/
+		
+		//add delete single item link
+		var deleteLink = document.createElement('a');
+		deleteLink.setAttribute("data-role", "button");
+		deleteLink.setAttribute("data-icon", "delete");
+		deleteLink.setAttribute("data-inline", "true");
+		deleteLink.setAttribute("data-theme", "b");
+		deleteLink.href = "#garage";
+		deleteLink.key = key;
+		var deleteText = "Delete Car";
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+
+
+	
+	function editCar() {
+		//Grab the data from our item from local storage
+		var value = localStorage.getItem(this.key);
+		var car = JSON.parse(value);
+		
+		//Show the form
+		toggleControls("off");
+		
+		//populate form fields with current values
+		ge('make').value = car.make[1];
+		ge('model').value = car.model[1];
+		ge('year').value = car.year[1];
+		ge('doors').value = car.doors[1];
+		ge('colors').value = car.colors[1];
+
+		var checkboxes = document.forms[0].display;
+		for (var i=0; i<car.display[1].length; i++) {
+			document.getElementById(car.display[1][i]).setAttribute("checked", "checked");
+		}
+		
+		var radios = document.forms[0].condition;
+		for (var i=0; i<radios.length; i++) {
+			if (radios[i].value == "Amazing" && car.condition[1] == "Amazing"){
+				radios[i].setAttribute("checked", "checked");
+			} else if (radios[i].value == "Not so amazing" && car.condition[1] == "Not so amazing") {
+				radios[i].setAttribute("checked", "checked");
+			} else if (radios[i].value == "Rubbish" && car.condition[1] == "Rubbish") {
+				radios[i].setAttribute("checked", "checked");
+			}
+		}
+		ge('describe').value = car.describe[1];
+		
+		//remove teh listener from input save button.
+		save.removeEventListener("click", storeData);
+		//Change submit button value to edit button
+		ge('headerBar').innerHTML = "Edit Car Tag";
+		ge('submit').value = "Edit Car Tag";
+		var editSubmit = ge('submit');
+		//Save the key value established in this function as a property of the editSubmit event
+		//so we can use that value when we save the data we edited.
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;
+	}
+	function deleteItem() {
+		var ask = confirm("Are you sure you want to delete this car.");
+		if (ask) {
+			localStorage.removeItem(this.key);
+			alert("Car was deleted!");
+			window.location.reload();
+		} else {
+			alert("Car was not deleted!");
+		}
+	}
+	
+	function clearLocal() {
+		if (localStorage.length === 0) {
+			alert("No cars to clear.")
+		} else {
+			localStorage.clear();
+			alert("All cars are deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
+	
+	function validate(e) {
+		//Define elements you want to check
+		var getMake = ge('make');
+		var getModel = ge('model');
+		var getYear = ge('year');
+		var getColor = ge('colors');
+		
+		//reset error messages
+		errMsg.innerHTML = "";
+		getMake.style.border = "none";
+		getModel.style.border = "none";
+		getYear.style.border = "none";
+		getColor.style.border = "none";
+
+		//get error messages
+		var messageAry  = [];
+		//Make Validation
+		if (getMake.value === "") {
+			var makeError = "Please enter a car make."
+			getMake.style.border = "1px solid red";
+			
+			messageAry.push(makeError);
+		}
+		//Model Validation
+		if (getModel.value === "") {
+			var modelError = "Please enter a car model."
+			getModel.style.border = "1px solid red";
+			messageAry.push(modelError);
+		}
+		//Year Validation
+		var re = /^\d{4}$/;
+		if (!re.exec(getYear.value)) {
+			var yearError = "Please enter a valid year.";
+			getYear.style.border = "1px solid red";
+			messageAry.push(yearError);
+		}
+		//If there are errors display them.
+		if (messageAry.length >= 1) {
+			for (var i=0, j=messageAry.length; i < j; i++) {
+				var txt = document.createElement('li');
+				txt.innerHTML = messageAry[i];
+				errMsg.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;
+		} else {
+			//if all is ok save data
+			storeData(this.key);
+		}
+
+	}
+	//Variable defaults
+	var errMsg = ge('errors');
+	pickColor(selectColor);
+	
+	//Set link and submit click events
+	
+
+	var displayLink = ge('displayLink');
+	displayLink.addEventListener("click", getData);
+
+/*
+	$("#garage").ready(function(){
+	getData();
+    alert('Your DOM is ready.Now below this u can run all ur javascript');
+    });
+*/
+	
+	var clearLink = ge('clear');
+	clearLink.addEventListener("click", clearLocal);
+	var save = ge('submit');
+	save.addEventListener("click", validate);
+	
+});
+
+
+
+
+
